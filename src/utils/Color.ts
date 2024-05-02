45c6d2b0257f8cf36e7cf1c9ff7482b2e901785f
soumen-pradhan <soumen.pradhan@infoedge.com>
2024-05-02 16:34:58 +0530

    add tailwind colors

---
+++ src/utils/Color.ts
@@ -0,0 +1,9 @@
+import * as THREE from 'three';
+
+/** From @link https://tailwindcss.com/docs/customizing-colors */
+
+export const SLATE_700 = new THREE.Color(0x334155);
+
+export const NEUTRAL_50 = new THREE.Color(0xfafafa);
+export const NEUTRAL_400 = new THREE.Color(0xa3a3a3);
+export const NEUTRAL_950 = new THREE.Color(0x0a0a0a);
