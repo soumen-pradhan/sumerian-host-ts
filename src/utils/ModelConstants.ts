73970cf34a09cace422018fd9a3bc15586c36d45
soumen-pradhan <soumen.pradhan@infoedge.com>
2024-05-07 14:35:54 +0530

    gltf props specific to the model

---
+++ src/utils/ModelConstants.ts
@@ -0,0 +1,8 @@
+const ModelConstants = {
+  audioAttachJoint1: 'chardef_c_neckB',
+  audioAttachJoint2: 'charhead',
+  lookJoint1: 'charjx_c_look',
+  lookJoint2: 'chargaze',
+};
+
+export default ModelConstants;
