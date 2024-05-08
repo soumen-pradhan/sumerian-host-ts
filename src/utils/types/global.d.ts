f3f87cdde76a57833d43ffc030ef1bdcbcf0f07d
soumen-pradhan <soumen.pradhan@infoedge.com>
2024-05-08 16:49:04 +0530

    Narrow viseme types

--- src/utils/types/global.d.ts
+++ src/utils/types/global.d.ts
@@ -5,6 +5,26 @@ declare global {
 
   type BlendMode = 'Override' | 'Additive';
 
+  type VisemeName =
+    | '@'
+    | 'a'
+    | 'e'
+    | 'E'
+    | 'f'
+    | 'i'
+    | 'k'
+    | 'o'
+    | 'O'
+    | 'p'
+    | 'r'
+    | 's'
+    | 'S'
+    | 'sil'
+    | 'stand_talk'
+    | 'T'
+    | 't'
+    | 'u';
+
   /** From {@link @tween-js/tween/dist/tween.d.ts} */
 
   type EasingFunction = (amount: number) => number;
