006e0441c133a066c517694a2e5f7e6b08b0cea9
soumen-pradhan <soumen.pradhan@infoedge.com>
2024-05-10 19:59:04 +0530

    update scene

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,21 @@ import './style.css';
 import './utils/Errors';
 
 import * as THREE from 'three';
-import { GLTF, GLTFLoader, OrbitControls } from 'three/examples/jsm/Addons.js';
+import {
+  DecalGeometry,
+  GLTF,
+  GLTFLoader,
+  OrbitControls,
+} from 'three/examples/jsm/Addons.js';
 import { RectAreaLightHelper } from 'three/addons/helpers/RectAreaLightHelper.js';
 import * as TWEEN from '@tweenjs/tween.js';
 
 import {
+  AMBER_100,
   AMBER_400,
+  GREEN_600,
   NEUTRAL_400,
-  SKY_200,
+  SKY_100,
   SLATE_50,
   SLATE_700,
   SLATE_950,
@@ -37,6 +44,8 @@ const paths = {
     lamp: 'assets/glTF/models/desk+lamp-gltf.glb',
     laptop: 'assets/glTF/models/S-Laptop.glb',
     banner: 'assets/glTF/models/Hanging_Banner.obj',
+    interior: 'assets/glTF/models/interior-empty.glb',
+    ceilingLamp: 'assets/glTF/models/ceiling_lamp.glb',
   },
   texture: {
     woodColor: 'assets/textures/worn_planks_diff_4k.jpg',
@@ -70,6 +79,11 @@ document.body.append(stats.dom);
 
   document.body.append(consentButton);
 })();
+
+// TODO Add cleanup
+// window.addEventListener('beforeunload', (e) => {
+// cleanup
+// });
 
 function test() {
   const sleep = (ms: number) => new Promise<void>((res) => setTimeout(() => res(), ms));
@@ -156,15 +170,28 @@ async function main() {
       });
   };
 
+  const showWireframe = (model: GLTF['scene'], show = true) => {
+    for (let child of model.children) {
+      if (child instanceof THREE.Mesh && child.isMesh) {
+        (child.material as { wireframe: boolean }).wireframe = show;
+      } else if (child instanceof THREE.Group && child.isGroup) {
+        showWireframe(child, show);
+      }
+    }
+  };
+
   addControls('Luke', charLuke).close();
+  // const lukeSkeleton = new THREE.SkeletonHelper(charLuke);
+  // scene.add(lukeSkeleton);
 
-  const [woodTexture] = await Promise.all([
-    new THREE.TextureLoader().loadAsync(paths.texture.woodColor),
-  ]);
+  // const [woodTexture] = await Promise.all([
+  //   new THREE.TextureLoader().loadAsync(paths.texture.woodColor),
+  // ]);
 
-  woodTexture.wrapS = THREE.RepeatWrapping;
-  woodTexture.wrapT = THREE.RepeatWrapping;
-  woodTexture.repeat.set(4, 4);
+  // woodTexture.wrapS = THREE.RepeatWrapping;
+  // woodTexture.wrapT = THREE.RepeatWrapping;
+  // woodTexture.repeat.set(4, 4);
+  // woodTexture.colorSpace = THREE.SRGBColorSpace;
 
   await Promise.all([
     addGlTFModel(paths.model.desk, 'Desk', (desk) => {
@@ -173,6 +200,7 @@ async function main() {
     addGlTFModel(paths.model.lamp, 'Lamp', (lamp) => {
       lamp.position.set(1.1, 1.07, 2.1);
       lamp.scale.set(0.04, 0.04, 0.04);
+      lamp.visible = false;
 
       const bulbMesh = lamp.children[0].children[2] as THREE.Mesh;
       (bulbMesh.material as THREE.MeshStandardMaterial).emissiveIntensity = 2.5;
@@ -181,6 +209,85 @@ async function main() {
       laptop.position.set(0, 0.28, 0.1);
       laptop.rotation.set(0, 3.3, 0);
       laptop.scale.set(1, 1, 1);
+
+      console.log(laptop);
+
+      // BigShyft Decal TODO Decal
+      {
+        const geom = new DecalGeometry(
+          laptop.children[0].children[1] as THREE.Mesh,
+          new THREE.Vector3(4, 4, 4),
+          new THREE.Euler(0, 0, 0),
+          new THREE.Vector3(1, 1, 1)
+        );
+        const mat = new THREE.MeshBasicMaterial({
+          color: GREEN_600,
+          depthWrite: false,
+        });
+        const decal = new THREE.Mesh(geom, mat);
+        decal.scale.set(10, 10, 10);
+        scene.add(decal);
+        console.log(decal);
+
+        const decalGui = gui.addFolder('Decal');
+        decalGui.add(decal.position, 'x');
+        decalGui.add(decal.position, 'y');
+        decalGui.add(decal.position, 'z');
+        decalGui.add(decal.rotation, 'x').name('Euler.x');
+        decalGui.add(decal.rotation, 'y').name('Euler.y');
+        decalGui.add(decal.rotation, 'z').name('Euler.z');
+        decalGui
+          .add(decal.scale, 'x')
+          .name('scale')
+          .onChange((v) => decal.scale.set(v, v, v));
+      }
+    }),
+    addGlTFModel(paths.model.interior, 'Interior', (room) => {
+      room.position.set(-2, 0, 2);
+      room.rotation.set(0, -1.57, 0);
+      gui.add({ wireframe: false }, 'wireframe').onChange((v) => showWireframe(room, v));
+
+      console.log(room);
+      room.children[3].receiveShadow = true; // Rug
+      room.children[4].receiveShadow = true; // Plane
+
+      const mat = new THREE.MeshPhysicalMaterial({
+        color: SKY_100,
+        transmission: 1,
+        roughness: 0.5,
+        thickness: 0,
+        metalness: 0.4,
+      });
+
+      const ceilingMatGui = gui.addFolder('Ceiling Mat');
+      ceilingMatGui.add(mat, 'transmission');
+      ceilingMatGui.add(mat, 'roughness');
+      ceilingMatGui.add(mat, 'thickness');
+      ceilingMatGui.add(mat, 'metalness');
+
+      const sphereMesh = room.children[2] as THREE.Mesh;
+      sphereMesh.material = mat;
+
+      const rimMesh = sphereMesh.children[0] as THREE.Mesh;
+      const rimMat = new THREE.MeshPhysicalMaterial({
+        color: SLATE_950,
+        transmission: 1,
+        roughness: 0,
+        thickness: 0,
+        metalness: 0.4,
+      });
+      rimMesh.material = rimMat;
+
+      const ceilingRimMatGui = gui.addFolder('Ceiling Rim Mat');
+      ceilingRimMatGui.add(rimMat, 'transmission');
+      ceilingRimMatGui.add(rimMat, 'roughness');
+      ceilingRimMatGui.add(rimMat, 'thickness');
+      ceilingRimMatGui.add(rimMat, 'metalness');
+    }),
+    addGlTFModel(paths.model.ceilingLamp, 'Ceiling Lamp', (ceiling) => {
+      ceiling.position.set(0, 2.5, 0);
+      ceiling.scale.set(0.2, 0.2, 0.2);
+      ceiling.visible = false;
     }),
     // new OBJLoader().loadAsync(paths.model.banner).then((banner) => {
     //   const left = banner.children[0] as THREE.Mesh;
@@ -191,17 +298,19 @@ async function main() {
     // }),
   ]);
 
-  groundMat.map = woodTexture;
-  woodTexture.needsUpdate = true;
-  woodTexture.colorSpace = THREE.SRGBColorSpace;
+  // groundMat.map = woodTexture;
+  // woodTexture.needsUpdate = true;
 
   const axesHelper = new THREE.AxesHelper(5);
-  scene.add(axesHelper);
+  const gridHelper = new THREE.GridHelper(10, 10);
+  scene.add(axesHelper, gridHelper);
 
+  // Lamp Light
   {
     const pointLight = new THREE.PointLight(AMBER_400, 4);
     pointLight.castShadow = true;
     pointLight.position.set(-0.45, 1.36, 0.53);
+    pointLight.visible = false;
 
     scene.add(pointLight);
 
@@ -220,9 +329,10 @@ async function main() {
     pointLightGui.close();
   }
 
+  // Wall of Light
   {
-    const rectLight = new THREE.RectAreaLight(SKY_200, 0, 40, 20);
-    rectLight.position.setX(-10);
+    const rectLight = new THREE.RectAreaLight(SKY_100, 10, 2.6, 1.9);
+    rectLight.position.set(-3.1, 1.4, -0.52);
     rectLight.rotation.set(0, -1.57, 0);
     rectLight.visible = false;
 
@@ -247,6 +357,50 @@ async function main() {
     rectLightGui.add(rectLight, 'intensity');
     rectLightGui.add(rectLight, 'visible');
     rectLightGui.close();
+  }
+
+  // Ceiling SemiCircle Light
+  {
+    const ceilLight = new THREE.PointLight(SKY_100, 10);
+    ceilLight.position.set(-0.38, 2.55, 0.4);
+    ceilLight.castShadow = true;
+
+    scene.add(ceilLight);
+
+    const ceiLightHelper = new THREE.PointLightHelper(ceilLight, 0.1, SLATE_50);
+    scene.add(ceiLightHelper);
+
+    const ceilLightGui = gui.addFolder('Ceil Circle Light');
+    ceilLightGui.add(ceilLight.position, 'x');
+    ceilLightGui.add(ceilLight.position, 'y');
+    ceilLightGui.add(ceilLight.position, 'z');
+    ceilLightGui.add(ceilLight, 'intensity');
+    ceilLightGui.add(ceilLight, 'visible').onChange((v) => (ceiLightHelper.visible = v));
+  }
+
+  // Standing lamp Light
+  {
+    const standingLampLight = new THREE.PointLight(AMBER_100, 10);
+    standingLampLight.position.set(1.23, 1.1, -2);
+    standingLampLight.castShadow = true;
+
+    scene.add(standingLampLight);
+
+    const standingLampHelper = new THREE.PointLightHelper(
+      standingLampLight,
+      0.1,
+      SLATE_50
+    );
+    scene.add(standingLampHelper);
+
+    const standingLightGui = gui.addFolder('standing Light');
+    standingLightGui.add(standingLampLight.position, 'x');
+    standingLightGui.add(standingLampLight.position, 'y');
+    standingLightGui.add(standingLampLight.position, 'z');
+    standingLightGui.add(standingLampLight, 'intensity');
+    standingLightGui
+      .add(standingLampLight, 'visible')
+      .onChange((v) => (standingLampHelper.visible = v));
   }
 
   const { host } = createHost({
@@ -391,7 +545,8 @@ function createScene() {
   //   dirLightTargetFolder.add(dirLightTarget.position, 'z', -N, N);
   // }
 
-  const ambientLight = new THREE.AmbientLight(SLATE_50, 1);
+  const ambientLight = new THREE.AmbientLight(SLATE_50, 0.5);
+  gui.add(ambientLight, 'intensity').name('ambient');
   scene.add(ambientLight);
 
   // Environment
