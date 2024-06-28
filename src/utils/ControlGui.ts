ffcc0a4533f78357ee5e2774b77f5f9f4a22a326
soumen-pradhan <soumen.pradhan@infoedge.com>
2024-06-28 23:20:12 +0530

    Add pos in ControlGui

--- src/utils/ControlGui.ts
+++ src/utils/ControlGui.ts
@@ -1,13 +1,21 @@
+import { Object3D } from 'three';
 import { GLTF } from 'three/examples/jsm/Addons.js';
 import GUI from 'three/examples/jsm/libs/lil-gui.module.min.js';
 
 export class ControlGui {
   #gui: GUI;
-  #model: GLTF['scene'];
+  #model: GLTF['scene'] | Object3D;
 
-  constructor(name: string, model: GLTF['scene'], localGui: GUI) {
+  constructor(name: string, model: GLTF['scene'] | Object3D, localGui: GUI) {
     this.#gui = localGui.addFolder(name);
     this.#gui.close();
     this.#model = model;
+  }
+
+  pos() {
+    this.#gui.add(this.#model.position, 'x');
+    this.#gui.add(this.#model.position, 'y');
+    this.#gui.add(this.#model.position, 'z');
+    return this;
   }
 }
