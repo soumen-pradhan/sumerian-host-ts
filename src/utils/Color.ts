9e08fcee42002acda616465e04c4e831720952ca
soumen-pradhan <soumen.pradhan@infoedge.com>
2024-06-28 22:30:42 +0530

    Add tailwind colors

--- src/utils/Color.ts
+++ src/utils/Color.ts
@@ -2,8 +2,27 @@ import * as THREE from 'three';
 
 /** From @link https://tailwindcss.com/docs/customizing-colors */
 
-export const SLATE_700 = new THREE.Color(0x334155);
+export const Color = {
+  SLATE_50: new THREE.Color(0xf8fafc),
+  SLATE_700: new THREE.Color(0x334155),
+  SLATE_950: new THREE.Color(0x020617),
 
-export const NEUTRAL_50 = new THREE.Color(0xfafafa);
-export const NEUTRAL_400 = new THREE.Color(0xa3a3a3);
-export const NEUTRAL_950 = new THREE.Color(0x0a0a0a);
+  ZINC_400: new THREE.Color(0xa1a1aa),
+
+  NEUTRAL_50: new THREE.Color(0xfafafa),
+  NEUTRAL_400: new THREE.Color(0xa3a3a3),
+  NEUTRAL_950: new THREE.Color(0x0a0a0a),
+
+  AMBER_100: new THREE.Color(0xfef3c7),
+  AMBER_400: new THREE.Color(0xfbbf24),
+
+  YELLOW_950: new THREE.Color(0x422006),
+
+  GREEN_600: new THREE.Color(0x16a34a),
+
+  SKY_100: new THREE.Color(0xe0f2fe),
+  SKY_200: new THREE.Color(0xbae6fd),
+  SKY_300: new THREE.Color(0x7dd3fc),
+
+  ROSE_600: new THREE.Color(0xe11d48),
+};
