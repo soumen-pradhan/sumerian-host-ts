7b5a48404011aee1ce1cf3396bcdbb262e9fa08b
soumen-pradhan <soumen.pradhan@infoedge.com>
2024-07-04 10:41:21 +0530

    Update exports

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,7 @@
 import type { IName } from './interface/IName';
 import Color from './Color';
 import { ControlGui } from './ControlGui';
+import { EmptyBase, uuid, getUniqueName } from './HostUtils';
+import AsciiUtils from './AsciiArt';
 
-export { IName, Color, ControlGui };
+export { IName, Color, ControlGui, EmptyBase, uuid, getUniqueName, AsciiUtils };
