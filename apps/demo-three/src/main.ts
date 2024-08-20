443f3914cf7975b458cb9b5e73b36e2685c815fd
soumen-pradhan <soumen.pradhan@infoedge.com>
2024-08-20 12:24:17 +0530

    Update the demo app

--- apps/demo-three/src/main.ts
+++ apps/demo-three/src/main.ts
@@ -1,1 +1,9 @@
+import './style.css';
+
+import { greet } from 'hosts-three';
+import * as THREE from 'three';
+
 console.log('Ave Caesar');
+greet();
+
+console.log(new THREE.Vector2(2, 3));
---
+++ apps/demo-three/src/utils/Color.ts
@@ -0,0 +1,30 @@
+import * as THREE from 'three';
+
+/** From @link https://tailwindcss.com/docs/customizing-colors */
+
+const Color = {
+  SLATE_50: new THREE.Color(0xf8fafc),
+  SLATE_700: new THREE.Color(0x334155),
+  SLATE_950: new THREE.Color(0x020617),
+
+  ZINC_400: new THREE.Color(0xa1a1aa),
+
+  NEUTRAL_50: new THREE.Color(0xfafafa),
+  NEUTRAL_400: new THREE.Color(0xa3a3a3),
+  NEUTRAL_950: new THREE.Color(0x0a0a0a),
+
+  AMBER_100: new THREE.Color(0xfef3c7),
+  AMBER_400: new THREE.Color(0xfbbf24),
+
+  YELLOW_950: new THREE.Color(0x422006),
+
+  GREEN_600: new THREE.Color(0x16a34a),
+
+  SKY_100: new THREE.Color(0xe0f2fe),
+  SKY_200: new THREE.Color(0xbae6fd),
+  SKY_300: new THREE.Color(0x7dd3fc),
+
+  ROSE_600: new THREE.Color(0xe11d48),
+};
+
+export default Color;
