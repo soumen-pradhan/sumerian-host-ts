f93d6ccb100e05d5282970e9d0682fe627c92bcf
soumen-pradhan <soumen.pradhan@infoedge.com>
2024-08-22 14:18:48 +0530

    Update hosts export

--- packages/hosts-core/src/Deferred.ts
+++ packages/hosts-core/src/Deferred.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 type State = 'pending' | 'resolved' | 'rejected' | 'cancelled';
 
 /**
--- packages/hosts-core/src/index.ts
+++ packages/hosts-core/src/index.ts
@@ -1,7 +1,9 @@
 import Messenger from './Messenger';
 import HostObject from './HostObject';
+import Deferred from './Deferred';
+import AbstractHostFeature from './AbstractHostFeature';
 
-export { Messenger, HostObject };
+export { Messenger, HostObject, Deferred, AbstractHostFeature };
 
 import type { IMessenger } from './Messenger';
 
